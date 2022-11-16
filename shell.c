a40de371ad631f2a3a9a0711b4dd1bdf897f6b44
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-11-16 15:54:44 -0800

    updated the shell file

--- shell.c
+++ shell.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+]
 /**
  * main - the shell program start point
  * @ac: int num of command line args
@@ -6,24 +7,25 @@
  * @ev: null term'd arr of strs contain env vars
  * Return: int result. 0 in success, everything else is an error
  */
+
 int main(int ac, char **av, char **ev)
 {
-        sev_t sev;
-        int exitcode = 0;
-        (void)ac;
-        init_shell_env(&sev, av, ev);
+	sev_t sev;
+	int exitcode = 0;
+	(void)ac;
+	init_shell_env(&sev, av, ev);
 
-        while (sev.skywalker)
-        {
-                dis_prompt(sev);
-                getcom(&sev);
-                checker_alias(&sev);
-                if (!check_builtin(&sev))
-                        actions(&sev);
-                dis_error(&sev);
-        }
-        write_log(&sev);
-        exitcode = sev.error;
-        cl_sev(&sev);
-        return (exitcode);
+	while (sev.skywalker)
+	{
+		dis_prompt(sev);
+		getcom(&sev);
+		checker_alias(&sev);
+		if (!check_builtin(&sev))
+			actions(&sev);
+		dis_error(&sev);
+	}
+	write_log(&sev);
+	exitcode = sev.error;
+	cl_sev(&sev);
+	return (exitcode);
 }
