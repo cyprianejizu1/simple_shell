2d391a52098cc44e6739d90d066a8a7369e7ccc1
A-Female-In-Tech <blesshow2019@gmail.com>
2022-11-17 20:40:25 +0100

    Tasks

--- shell.h
+++ shell.h
@@ -1,42 +1,11 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 /*
  * File: shell.h
  * Auth: Alex Yu
  *       Brennan D Baraban
  */
-
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/resource.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include "./stringfile/string.h"
-#include "linklist.h"
-#include "./memofile/memo.h"
-#include "shellvar.h"
-#include "./builtinfile/builtin.h"
-#include "./errorfile/error.h"
-#include "./shellhelperfile/shellhelper.h"
-#include "log.h"
-#define SPACE " "
-#define EQUAL "="
-#define COLON ":"
-#define BSLASH "\\"
-#define FSLASH "/"
-#define BUF_SIZE 4096
-#define PS1 write(STDOUT_FILENO, "MANDALORIAN$ ", 4)
-#define PS2 write(STDOUT_FILENO, "\nMANDALORIAN$ ", 5)
-#define NEWLINE write(STDOUT_FILENO, "\n", 1)
-#define DELIM " \t"
 
 #include <fcntl.h>
 #include <signal.h>
@@ -169,27 +138,4 @@ void help_unsetenv(void);
 void help_history(void);
 
 int proc_file_commands(char *file_path, int *exe_ret);
-
-
-/* _getline.c */
-ssize_t _getline(char **lineptr, size_t *n, int fd, list_t **mt);
-void reset_buffer(char *buffer, ssize_t buf_size);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_strdup(char *str, list_t **mt);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strtok(char *s, const char *delim);
-unsigned int _strlen(char *s);
-
-/* string02.c */
-int _strcmp(char *s1, char *s2);
-int _atoi(char *s);
-char *_strcat(char *s1, char *s2, list_t **mt);
-char *_itoa(ssize_t num, list_t **mt);
-char *reverse_str(char *s);
-
-/* string03.c */
-char *_strchr(char *s, char c);
-void _chartostr(char *s, char c);
-
 #endif
